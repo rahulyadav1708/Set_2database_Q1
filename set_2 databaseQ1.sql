3dffe0687b1bca117651724695de5425bfc7869b
rahulyadav1708 <141802799+rahulyadav1708@users.noreply.github.com>
2024-08-03 13:34:07 +0530

    Add files via upload

---
+++ set_2 databaseQ1.sql
@@ -0,0 +1,17 @@
+create table Person(
+ id int Primary key,
+ email varchar(100) not null
+ );
+
+ select * from Person
+
+ insert into Person values(1,'rah@123.com');
+ insert into Person values(2,'mah@123.com');
+ insert into Person values(3,'roh@345.com');
+ insert into Person values(4,'pra@567.com');
+ insert into Person values(5,'rah@123.com');
+
+SELECT email, COUNT(id)
+FROM person
+GROUP BY email
+HAVING COUNT(email) > 1;
